4e5d15ba01cc1c56c2e01a5a095fee304ac4edcb
Naash <dgmg@outlook.fr>
2022-04-01 12:43:51 +0200

    github sync 1.1

--- app/Http/Controllers/AnnoncesController.php
+++ app/Http/Controllers/AnnoncesController.php
@@ -17,7 +17,7 @@ class AnnoncesController extends Controller
      */
     public function index()
     {
-        //On récupère tous les annonces de la table annonces
+        //On récupère tous les annonces de la table annonce
         $annonces = DB::table('annonces')->join('biens_appartements', 'annonces.bien_id', '=', 'biens_appartements.id')->get();
         
         //On retourne les annonces à la vue
